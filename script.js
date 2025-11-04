23ff077b3eb04e0a602c507d441a316d9291e9a9
Luiz Felipe <ganassini.luiz@escola.pr.gov.br>
2025-11-04 10:20:34 -0300

    04/11

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ function mostraPalavrasChave(){
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
 
+    for (let i in palavras){
+        palavras[i].toLowerCase();
+    }
+    palavras = tiraPalavrasRuins(palavras);
+
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
@@ -41,4 +46,16 @@ function contaFrequencias(palavras){
 
     return palavras;
 
+}
+
+function tiraPalavrasRuins(palavras){
+    const PALAVRAS_RUINS = new Set(["para", "nós", "das", "como", "que"]);
+    const palavrasBoas = [];
+
+    for (let palavras of palavras){
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2){
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
